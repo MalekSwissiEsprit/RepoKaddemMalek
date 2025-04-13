bb9b5e8b503e8364d46baf1620074b28cc8b70ac
MalekSwissiEsprit <malekswissi11@gmail.com>
2025-04-13 03:55:37 +0100

    add univ9

--- src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.entities.Universite;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
@@ -24,6 +26,7 @@ import static org.mockito.Mockito.*;
 //order travailler
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 //une classe de test
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class UniversiteServiceImplTest {
     @Mock
